6603d2a7f1adcdd693865af171baf6f24f93c12e
Lucas Juan <ljgsodre@outlook.com>
2022-12-11 19:58:59 -0300

    Add project files.

---
+++ Adapter_Pattern/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Adapter_Pattern;
+
+Adaptee adaptee = new Adaptee();
+var target = new Adapter(adaptee);
+
+Console.WriteLine("Adaptee interface is incompatible with the client.");
+Console.WriteLine("But with adapter client can call it's method.");
+
+Console.WriteLine(target.GetRequest());
+
---
+++ Adapter_PatternTests/AdapterTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Adapter_Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern.Tests
+{
+    [TestClass()]
+    public class AdapterTests
+    {
+        [TestMethod()]
+        public void AdapterTest()
+        {
+            Adaptee adaptee = new Adaptee();
+            var target = new Adapter(adaptee);
+            var result = target.GetRequest();
+            Assert.AreEqual<string>(result, "This is 'Specific request.'");
+           
+        }
+    }
+}
